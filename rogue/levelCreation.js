b51d385a8e9d4f6aff51912095e6c47f94f5938c
Pedro Rabinovitch <pedro@spaceapegames.com>
2012-11-06 23:25:50 +0000

    Wall lengths, copying box colour

--- rogue/levelCreation.js
+++ rogue/levelCreation.js
@@ -1,7 +1,10 @@
 pc.script.create('levelCreation', function (context) {
     var ROOT_WALL_NAME = 'Wall0';
+    var ROOT_WALL_CONTAINER = 'Walls';
     var rootWall;
-    
+    var rootWallContainer;
+    var CELL_TO_WORLD = 10;
+       
     var LevelCreation = function (entity) {
         this.entity = entity;
     };
@@ -9,24 +12,32 @@ pc.script.create('levelCreation', function (context) {
     LevelCreation.prototype = {
         initialize: function () {
             rootWall = context.root.findByName(ROOT_WALL_NAME);
-            this.addWall(-8, 2, 5, true);
+            rootWallContainer = context.root.findByName(ROOT_WALL_CONTAINER);
+            this.addWall(-2, 2, 2, true);
+            this.addWall(-2, 2, 4, false);
         },
         
         addWall: function(x, z, length, vertical) {
             var newWall = new pc.fw.Entity();
                         
+            var boxColor = context.systems.primitive.get(rootWall, 'color');
             context.systems.primitive.createComponent(newWall, {
                 type: 'Box',
-                color: '0xff0000',
+                color: boxColor,
             });
  
             var rootY = rootWall.getLocalPosition()[1];
-            newWall.setLocalPosition(x, rootY, z);
-            newWall.setLocalScale(rootWall.getLocalScale());
+            var rootScale = rootWall.getLocalScale();
+            newWall.setLocalPosition(x*CELL_TO_WORLD, rootY, z*CELL_TO_WORLD);
             if (vertical)
                 newWall.rotateLocal(0, 90, 0);
+            newWall.translateLocal(rootScale[0]/2, 0, rootScale[2]/2);
+            if (length > 1) {
+	            rootScale[0] *= length;
+            }
+            newWall.setLocalScale(rootScale);
             
-            context.root.addChild(newWall);
+            rootWallContainer.addChild(newWall);
         },
         
         update: function (dt) {
