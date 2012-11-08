ff6b43c9c546da4d6f97d357aa313b6163aac319
Pedro Rabinovitch <pedro@spaceapegames.com>
2012-11-08 09:07:15 +0000

    Enclosed rooms!

--- rogue/levelCreation.js
+++ rogue/levelCreation.js
@@ -6,7 +6,13 @@ pc.script.create('levelCreation', function (context) {
     var rootTile;
     var rootTileContainer;
     var CELL_TO_WORLD = 10;
-       
+    var level;
+    var ROWS = 20;
+    var COLS = 20;
+    var UNDEFINED = 1;
+    var HOLLOW = 0;
+    var BLOCKED = 2;
+           
     var LevelCreation = function (entity) {
         this.entity = entity;
     };
@@ -35,16 +41,31 @@ pc.script.create('levelCreation', function (context) {
             this.addWall(-2, 2, 2, true);
             this.addWall(-2, 2, 2, false);
             
-            for (var x = -5; x <= 5; x++)
-	            for (var z = -5; z <= 5; z++)
-		            this.addTile(x, z, 0);//5*Math.cos(x*z*0.07));
+            this.createMaze();
+            
+            for (var z=0; z < COLS; z++) {
+	            for (var x=0; x < COLS; x++) {
+	            	if (level[z][x] == HOLLOW) {
+		            	this.addTile(x, z, level[z][x]);
+		            	if (level[z][x+1] != HOLLOW)
+		            		this.addWall(x+1, z+1, 1, true);
+		            	if (level[z][x-1] != HOLLOW)
+		            		this.addWall(x, z+1, 1, true);
+		            	if (level[z-1][x] != HOLLOW)
+		            		this.addWall(x, z, 1, false);
+		            	if (level[z+1][x] != HOLLOW)
+		            		this.addWall(x, z+1, 1, false);
+		            }
+	            }
+            }
+            
         },
         
         addTile: function(x, z, height) {
         	var newTile = cloneEntity(rootTile);
             var rootY = rootTile.getLocalPosition()[1];
             var rootScale = rootTile.getLocalScale();
-        	newTile.setLocalPosition(x*CELL_TO_WORLD, rootY, z*CELL_TO_WORLD);
+        	newTile.setLocalPosition((x-COLS/2)*CELL_TO_WORLD, rootY, (z-ROWS/2)*CELL_TO_WORLD);
         	newTile.translateLocal(rootScale[0]/2, height, rootScale[2]/2);
             rootTileContainer.addChild(newTile);
 
@@ -54,7 +75,7 @@ pc.script.create('levelCreation', function (context) {
  
             var rootY = rootWall.getLocalPosition()[1];
             var rootScale = rootWall.getLocalScale();
-            newWall.setLocalPosition(x*CELL_TO_WORLD, rootY, z*CELL_TO_WORLD);
+            newWall.setLocalPosition((x-COLS/2)*CELL_TO_WORLD, rootY, (z-ROWS/2)*CELL_TO_WORLD);
             if (vertical)
                 newWall.rotateLocal(0, 90, 0);
             if (length > 1) {
@@ -68,7 +89,41 @@ pc.script.create('levelCreation', function (context) {
         },
         
         update: function (dt) {
-        }
+        },
+        
+        createMaze: function() {
+        	level = new Array(ROWS);
+        	for (var z=0; z < ROWS; z++) {
+        		level[z] = new Array(COLS);
+        		for (var x=0; x < COLS; x++)
+        			level[z][x] = UNDEFINED;
+        	}
+        	
+//         	var sx = Math.floor(pc.math.random(0, COLS));
+//         	var sz = Math.floor(pc.math.random(0, ROWS));
+//         	var spawn = pc.math.vec2.create(sx,sz);
+//         	level[sz][sx] = HOLLOW;
+        	
+        	this.carveRoom(5, 12, 6, 4);
+        	this.carveRoom(10, 4, 4, 5);
+        },
+        
+        carveRoom: function(x0, z0, dx, dz) {
+        	this.fillArea(x0, z0, dx, dz, HOLLOW, false);
+        	this.fillArea(x0-1, z0-1, dx+2, 1, BLOCKED, true);
+        	this.fillArea(x0-1, z0+dz, dx+2, 1, BLOCKED, true);
+        	this.fillArea(x0-1, z0-1, 1, dz+2, BLOCKED, true);
+        	this.fillArea(x0+dx, z0-1, 1, dz+2, BLOCKED, true);
+        },
+        
+        fillArea: function(x0, z0, dx, dz, type, ifUndefined) {
+        	for (var z = z0; z < z0+dz; z++) {
+	        	for (var x = x0; x < x0+dx; x++) {
+	        		if (!ifUndefined || level[z][x] == UNDEFINED)
+		        		level[z][x] = type;
+	        	}        	
+        	}
+        },
     };
 
    return LevelCreation;
